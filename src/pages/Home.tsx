d356289bc24ad4c184e5f67a162d92c084304c7d
Alissa Sousa <alissiasuelen@hotmail.com>
2026-06-16 20:50:31 -0300

    feat: implement weekly highlights section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,11 @@
+import { WeeklyHighlights } from "../components/highlights/WeeklyHighlights";
 
+export function Home() {
+  return (
+    <main className="min-h-screen w-full overflow-x-hidden bg-page-bg text-text">
+      <div className="mx-auto min-h-screen w-[calc(100%-32px)] bg-neutral-900 md:w-[calc(100%-80px)] lg:w-[calc(100%-142px)]">
+        <WeeklyHighlights />
+      </div>
+    </main>
+  );
+}
